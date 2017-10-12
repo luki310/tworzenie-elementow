f7ea6106d13c78abc8cd18a822c455e5ba872e26
Łukasz Kuczek <lkuczek@wp.pl>
2017-10-12 02:03:10 +0200

    Ready

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
 (function($) {
     $(document).ready(function() {
+        var button = $("button"),
+            input = $("#inputUser, .error-mess"),
+            divUl = $(".col-6.list"),
+            ul = $("<ul></ul>", {
+                    "class": "list-group"
+                });
 
+        button.on("click", function(e){
+            e.stopPropagation();
+
+            var user = $.trim(input.val());
+
+            if (user === "") {
+                input.toggleClass("error");
+            } else {
+                var li = $("<li></li>", {
+                    "text": user,
+                    "class": "list-group-item"
+                });
+                if (divUl.children().length == 0) ul.appendTo(divUl);
+                li.appendTo("ul.list-group");
+                input.val("");
+            }
+        })
     });
 })(jQuery);
